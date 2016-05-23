513b0f31f9cfdfb87ba6a53ff7fbc6fe3f8e27f8
vladimirivkovic <vlado.ivkovic@live.com>
2016-05-23 21:23:15 +0200

    comments api authorization index.html at home

---
+++ TicketingSystem/TicketingSystem/DTOs/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketingSystem.DTOs
+{
+    public class UserDTO
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String UsedID { get; set; }
+    }
+}
